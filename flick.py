189ec71309ca28e96c832c86691173d05920fe45
Anshul Kushwaha <anshulkushwaha1@gmail.com>
2013-09-13 21:15:29 +0530

    traverse

--- flick.py
+++ flick.py
@@ -1,32 +1,34 @@
 import tkinter as tk
-import os
 import re
-import urllib.request
-import urllib.parse
-
+##import urllib.request
+##import urllib.parse
+import os
 
 root = tk.Tk()
 root.withdraw()
 
 def do():
-    print("In directory " + os.getcwd())
+##    print("In directory " + os.getcwd())
     for file in os.listdir('.'):
         if os.path.isdir(file):
             os.chdir(file)
             do()
-        elif os.stat(file).st_size >= 50*1024*1024:
-#            print("File: " + file )
+##        if os.stat(file).st_size >= 50*1024*1024:
+            print("File: " + file )
             fn = re.sub(r'[^a-zA-Z0-9]', ' ',file[:-4])
             print(fn)
-            response = urllib.request.urlopen('http://www.omdbapi.com/?t=' + urllib.parse.quote(fn))
-            print(urllib.parse.quote(fn))
-            omdb = response.read()
-            omdb.decode('utf-8')
-#            om = omdb.split("\"imdbRating\":\"",1)
-            print(omdb)
-
-        if os.getcwd() == dir:
-            os.chdir('..')
+##            response = urllib.request.urlopen('http://www.omdbapi.com/?t=Inception')
+##                        #            print(urllib.parse.quote(fn))
+##            omdb = response.read()
+##            omdb = omdb.decode("utf-8")
+##            om = omdb.split("\"imdbRating\":\"",1)
+##            print(om[1][:3])
+##
+##        print(re.sub(r'[^a-zA-Z0-9]', ' ',os.getcwd()))
+##        print(re.sub(r'[^a-zA-Z0-9]', ' ',dir))
+        if re.sub(r'[^a-zA-Z0-9]', ' ',os.getcwd()) != re.sub(r'[^a-zA-Z0-9]', ' ',dir):
+                os.chdir('..')
+            
     return
 
 dir = tk.filedialog.askdirectory()
